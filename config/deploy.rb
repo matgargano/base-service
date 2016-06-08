766e3453258d0e00caad59ec7c56bb3f40ab0026
Circle CI <circleci@forthepeople.com>
2016-06-08 09:51:20 -0400

    add capistrano

---
+++ config/deploy.rb
@@ -0,0 +1,26 @@
+# config valid only for current version of Capistrano
+
+
+set :application, 'service.statenweb.com'
+set :repo_url, 'git@github.com:matgargano/base-service.git'
+set :log_level, :info
+set :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+
+set :linked_files, fetch(:linked_files, []).push('.env')
+
+
+
+
+namespace :deploy do
+  desc 'Restart application'
+  task :restart do
+    on roles(:app) do
+    	within release_path do
+	        execute :composer, :install
+
+	  end
+    end
+  end
+end
+
+after 'deploy:publishing', 'deploy:restart'
